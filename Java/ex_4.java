2b17ab98781730735b75c1f64858e878a4ac4c00
YohanPark <113326892+yohan-j-park@users.noreply.github.com>
2022-09-17 23:45:37 +0900

    Add files via upload

---
+++ Java/ex_4.java
@@ -0,0 +1,17 @@
+
+public class ex_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 100;
+		double pi = 3.14;
+		char ch = 'a';  // char 에는 문자 하나만 저장할 수 있다.
+		String str = "abc"; // String에는 문자열을 저장할 수 있다.
+		
+		System.out.println(x);
+		System.out.println(pi);
+		System.out.println(ch);
+		System.out.println(str);
+	}
+
+}
