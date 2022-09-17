0ddd0a6fe651cb50566372c4b78a60adcbe5bef8
YohanPark <113326892+yohan-j-park@users.noreply.github.com>
2022-09-17 22:49:23 +0900

    Java0917

---
+++ Java/ex_2.java
@@ -0,0 +1,13 @@
+
+public class ex_2 {
+	public static void main(String[] args) { // 어플리케이션이 실행되면 제일 먼저 메인 메소드를 실행한다.
+		System.out.println("Hello,World");
+		System.out.print("3+5="); 	 //화면에 3+5를 출력하고 줄바꿈 안함.
+		System.out.println(3+5); 	 // 화면에 8이 출력됨
+		System.out.println(5+3);     // 화면에 5+3의 결과인 8이 출력된다.
+		System.out.println(5-3);     // 화면에 5-3의 결과인 2가 출력된다.
+		System.out.println(5*3);     // 화면에 5*3의 결과인 15가 출력된다.
+		System.out.println(5/3);     // 화면에 5/3의 결과인 1이 출력된다.
+	}
+
+}
