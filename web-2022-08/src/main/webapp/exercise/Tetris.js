5c5ddd367a269c825ed2925fd4a5f693f5040ed8
YohanPark <113326892+yohan-j-park@users.noreply.github.com>
2022-10-08 22:58:00 +0900

    2022-10-08

--- web-2022-08/src/main/webapp/exercise/Tetris.js
+++ web-2022-08/src/main/webapp/exercise/Tetris.js
@@ -1,9 +1,12 @@
 /**
- 10/3 2일차 미완성
+ 10/4 3일차 완성
  */
+import blocks from './blocks.js'
 // DOM
 let playground = document.querySelector(".playground > ul");
-
+const gameText = document.querySelector(".game-text");
+const scoreDisplay = document.querySelector(".score");
+const restartButton = document.querySelector(".game-text > button");
 
 // SETTING
 let game_rows = 20;
@@ -15,17 +18,11 @@ let score = 0;
 let duration = 500;		//블럭이 떨어지는 시간
 let downInterval; 		//최대한 null값?
 let tempMovingItem;		// 무빙을 실행하기 전에 담아두는 용도
-const blocks = {
-	Tshape: [
-		[[2,1],[0,1],[1,0],[1,1]], // [] 안의 []는 방향키를 눌렀을때 변하는 좌표값
-		[],
-		[],
-		[],
-	]
-}
+let isFalling = false;
+
 const MovingItem = {	// 다음 블럭의 타입과 좌표들의 정보를 담는 변수
-	type: "Tshape",
-	direction: 0, 		// 화살표 방향키를 눌렀을때 좌우로 돌리는 지표
+	type: "",
+	direction: 3, 		// 화살표 방향키를 눌렀을때 좌우로 돌리는 지표
 	top: 0,				// 좌표 기준으로 어디까지 내려가야 되는지 표현해주는 것
 	left: 3,		// 좌표 기준으로 어디까지 옆으로 움직일 수 있는지 표현해주는 것	
 };
@@ -41,7 +38,7 @@ function init(){     /* 화면이 처음 실행될 때 실행되는 함수 이�
 	for (let i=0; i< game_rows; i++){
 		prependNewLine()
 	}	
-	renderBlocks()
+	generateNewBlock()
 }
 
 function prependNewLine() {
@@ -49,42 +46,112 @@ function prependNewLine() {
 	const ul = document.createElement("ul");
 	for (let j=0; j<game_cols; j++){
 		
-	let matrix = document.createElement("li");
+	const matrix = document.createElement("li");
 		ul.prepend(matrix);
 	}
 	li.prepend(ul)
 	playground.prepend(li)
 }
 
-function renderBlocks(){	//블럭을 좌표값에 맞게 그리는 함수
+function renderBlocks(moveType = ""){	//블럭을 좌표값에 맞게 그리는 함수
 	const { type, direction, top, left} = tempMovingItem;
 	const movingBlocks = document.querySelectorAll(".moving");
-	movingBlocks.forEach(moving=>{
+	movingBlocks.forEach(moving =>{
 		moving.classList.remove(type, "moving");
 	})
-	blocks[type][direction].forEach(block=>{
+	
+	blocks[type][direction].some(block=>{
 		const x = block[0] + left; // ul안에 들어있는 li의 값
 		const y = block[1] + top; // li의 row값
 		const target = playground.childNodes[y]? playground.childNodes[y].childNodes[0].childNodes[x] : null;
 		const isAvailable = checkEmpty(target);
 		if(isAvailable){
-			target.classList.add(type, "moving")
+			target.classList.add(type, "moving") // 명시 된 클래스를 추가하는 매서드
 		} else{
 			tempMovingItem = {...MovingItem}
-			renderBlocks()
+			if(moveType === 'retry'){
+				clearInterval(downInterval);
+				showGameoverText()
+			}
+			setTimeout(()=>{
+				renderBlocks('retry')
+				if(moveType === "top"){
+					seizeBlock();
+				}
+			
+			},0)
+			return true;
 		}
-		target.classList.add(type, "moving") // 명시 된 클래스를 추가하는 매서드
+		
 	})
+	MovingItem.left = left;
+	MovingItem.top = top;
+	MovingItem.direction = direction;
 }
-function checkEmpty(taget){
-	if(!taget){
+function seizeBlock(){ // 바닥까지 내려 갔을 때 더 이상 내려가지 않게 하는 함수
+	const movingBlocks = document.querySelectorAll(".moving");
+	movingBlocks.forEach(moving=>{
+		moving.classList.remove("moving");
+		moving.classList.add("seized");
+	})
+	checkMatch()
+	
+}
+function checkMatch(){
+	const childNodes = playground.childNodes;
+	childNodes.forEach(child=>{
+		let matched = true;
+		child.children[0].childNodes.forEach(li=>{
+			if(!li.classList.contains("seized")){
+				matched = false;
+			}
+		})
+		if(matched){
+			child.remove();
+			prependNewLine()
+			score++;
+			scoreDisplay.innerText = score;
+		}
+	})
+	generateNewBlock()
+}
+function generateNewBlock(){
+	clearInterval(downInterval);
+	downInterval = setInterval(() => {
+		moveBlock('top', 1)
+	},duration)
+	const blockArray = Object.entries(blocks);
+	const randomIndex = Math.floor(Math.random() * blockArray.length);
+	MovingItem.type = blockArray[randomIndex][0];
+	MovingItem.top = 0;
+	MovingItem.left = 3;
+	MovingItem.direction = 0;
+	tempMovingItem = {...MovingItem};
+	renderBlocks()
+}
+function checkEmpty(target){
+	if(!target || target.classList.contains("seized")){
 		return false;
 	}
 	return true;
 }
 function moveBlock(moveType, amount){
 	tempMovingItem[moveType] += amount;
+	renderBlocks(moveType)
+}
+function changeDirection(){
+	const direction = tempMovingItem.direction;
+	direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1;
 	renderBlocks()
+}
+function dropBlock(){
+	clearInterval(downInterval);
+	downInterval = setInterval(() => {
+		moveBlock("top",1)
+	},10)
+}
+function showGameoverText(){
+	gameText.style.display = "flex"
 }
 // event handling
 document.addEventListener("keydown", e=>{
@@ -98,8 +165,18 @@ document.addEventListener("keydown", e=>{
 		case 40: //40: down
 			moveBlock("top", 1);
 			break;
-			
+		case 38:
+			changeDirection();
+			break;	
+		case 32:
+			dropBlock();
+			break;
 		default:
 			break;
 	}
+})
+restartButton.addEventListener("click",()=>{
+	playground.innerHTML = "";
+	gameText.style.display = "none"
+	init()
 })
