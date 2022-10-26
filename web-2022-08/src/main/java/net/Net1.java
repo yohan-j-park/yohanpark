38a4912d24b102d2cade14120ab3b33986a03d1e
YohanPark <113326892+yohan-j-park@users.noreply.github.com>
2022-10-26 23:27:14 +0900

    2022-10-26 Class

---
+++ web-2022-08/src/main/java/net/Net1.java
@@ -0,0 +1,25 @@
+package net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+public class Net1 {
+    
+    public static void main(String[] args){
+        System.out.println("Local Host");
+        try {
+            InetAddress n1 = InetAddress.getLocalHost();
+            System.out.println("HostName: " + n1.getHostName());
+            System.out.println("address: " + n1.getHostAddress());
+            
+            System.out.println("Network Host");
+            InetAddress n2 = InetAddress.getByName("www.google.com");
+            System.out.println("HostName: " + n2.getHostName());
+            System.out.println("address: " + n2.getHostAddress());
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
