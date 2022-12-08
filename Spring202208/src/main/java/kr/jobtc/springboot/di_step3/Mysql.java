5bf23caf0bedfd79144908441216c8c5ea02a62e
YohanPark <113326892+yohan-j-park@users.noreply.github.com>
2022-12-08 18:04:49 +0900

    2022-12-08 Class

--- Spring202208/src/main/java/kr/jobtc/springboot/di_step3/Mysql.java
+++ Spring202208/src/main/java/kr/jobtc/springboot/di_step3/Mysql.java
@@ -4,6 +4,15 @@ public class Mysql implements Sql{
 
 	public Mysql() {
 		System.out.println("Mysql");
+		long beforeTime = System.currentTimeMillis();
+		try {
+			Thread.sleep(1000);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		long afterTime = System.currentTimeMillis();
+		long secDiffTime = (afterTime - beforeTime);
+		System.out.println("시간차이(m) : " + secDiffTime);
 	}
 	
 	@Override
